c6f98b71dfdf95d96cb3b679e3180c4b7e06790d
vbragaesilva <nickblack141499@gmail.com>
2021-02-08 16:30:44 +0000

    init

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Bases Calc</title>
+      </Head>
+    </div>
+  )
+}
